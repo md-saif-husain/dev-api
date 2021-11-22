b849772554e961be7deed609f1813864ba22f59d
Saif Husain <saif.husain@experian.com>
2021-11-22 19:42:32 +0530

    (refactor):convert to single lib and multiple binary

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use devapi::run;
 
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    run().await
 }
