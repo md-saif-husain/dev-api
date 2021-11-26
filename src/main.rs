e98de1d6ee6cbd3d416af0059e7346077642ffd3
saif.husain <saif.husain@experian.com>
2021-11-26 01:57:30 -0500

    test:refactor to use dynamic port

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use devapi::run;
+use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("0.0.0.0:3000").expect("Failed to bind random port");
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,18 +1,19 @@
 //! tests/health_check.rs
+use std::net::TcpListener;
 
 // You can inspect what code gets generated using
 // `cargo expand --test health_check` (<- name of the test file)
 #[actix_rt::test]
 async fn health_check_works() {
     //Arrange
-    spawn_app();
+    let address = spawn_app();
     // We need to bring in `reqwest`
     // to perform HTTP requests against our application.
     let client = reqwest::Client::new();
 
     //Act
     let response = client
-        .get("http://127.0.0.1:8080/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to call health check endpoint");
@@ -22,11 +23,12 @@ async fn health_check_works() {
 }
 
 // Launch our application in the background
-fn spawn_app() {
-    let server = devapi::run().expect("Failed to bind address");
-    // Launch the server as a background task
-    // tokio::spawn returns a handle to the spawned future,
-    // but we have no use for it here, hence the non-binding let
-    //
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+    let server = devapi::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
+    // We return the application address to the caller!
+    format!("http://127.0.0.1:{}", port)
 }
