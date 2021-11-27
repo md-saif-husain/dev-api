4e90d0a8a3044b7132cfa8b6e6144776bad56660
MD SAIF  HUSAIN <70682968+md-saif-husain@users.noreply.github.com>
2021-11-27 21:06:01 +0530

    Post API to save user subscription (#10)
    * (feat):add route for subscription api
    * (feat):set up sqlx db migration
    * (feat):refactor lib and configuration
    * (feat):refactor add config to read configuration
    * (feat):implement DatabaseSetting to get connection string
    * (test): refactor add db connection in integration test
    * (refactor):revert GitHub action workflow changes
    * (refactor):update ci workflow to create db for integration test
    * (refactor):update build.yml
    *(refactor): update general.yml
    *(fix):fix lint issue
    * (refactor):revert GitHub action
    * (fix): give permission to init_db script to fix failing GitHub action
    * (fix):update workflow to fix build
    * (fix):update sqlx-data.json to fix failing build
    * (fix):update to use correct password 
    * (feat): save POSTED subscription to db
    *(test): integration test to save subscription
    *(test): refactor to create database for each test run
    *(fix): fix fmt error to pass build
    * (fix): build by passing updated sqlx-data.json
    
    Co-authored-by: saif.husain <saif.husain@experian.com>

---
+++ src/routes/subscription.rs
@@ -0,0 +1,34 @@
+use actix_web::{web, HttpResponse};
+use serde::Deserialize;
+use sqlx::PgPool;
+
+#[derive(Deserialize)]
+#[allow(dead_code)]
+pub struct FormData {
+    email: String,
+    name: String,
+}
+
+pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    // `Result` has two variants: `Ok` and `Err`.
+    // The first for successes, the second for failures.
+    // We use a `match` statement to choose what to do based
+    // on the outcome.
+    // We will talk more about `Result` going forward!
+    match sqlx::query!(
+        r#"INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)"#,
+        uuid::Uuid::new_v4(),
+        form.email,
+        form.name,
+        chrono::Utc::now()
+    )
+    .execute(pool.get_ref())
+    .await
+    {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => {
+            println!("Failed to execute query: {}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
