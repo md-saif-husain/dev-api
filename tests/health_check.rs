2e1590b1f82e5a394ac454f8f12ccf2c9f9a3b7c
saif.husain <saif.husain@experian.com>
2021-11-27 09:59:26 -0500

    test: refactor to create databse for each test run

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,80 +1,101 @@
-//! tests/health_check.rs
-use devapi::configuration::get_configuration;
-use sqlx::PgPool;
+use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
+use uuid::Uuid;
+use devapi::configuration::{get_configuration, DatabaseSettings};
+use devapi::startup::run;
 
 pub struct TestApp {
     pub address: String,
     pub db_pool: PgPool,
 }
 
-// Launch our application in the background
 async fn spawn_app() -> TestApp {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     // We retrieve the port assigned to us by the OS
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
-    let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect db when running test");
 
-    let server =
-        devapi::startup::run(listener, connection_pool.clone()).expect("Failed to bind address");
+    let mut configuration = get_configuration().expect("Failed to read configuration.");
+    configuration.database.database_name = Uuid::new_v4().to_string();
+    let connection_pool = configure_database(&configuration.database).await;
+
+    let server = run(listener, connection_pool.clone()).expect("Failed to bind address");
     let _ = tokio::spawn(server);
-    // We return the TestApp instance to the caller!
     TestApp {
         address,
         db_pool: connection_pool,
     }
 }
 
-// You can inspect what code gets generated using
-// `cargo expand --test health_check` (<- name of the test file)
+pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
+    // Create database
+    let mut connection = PgConnection::connect(&config.connection_string_without_db())
+        .await
+        .expect("Failed to connect to Postgres");
+    connection
+        .execute(&*format!(r#"CREATE DATABASE "{}";"#, config.database_name))
+        .await
+        .expect("Failed to create database.");
+
+    // Migrate database
+    let connection_pool = PgPool::connect(&config.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate the database");
+
+    connection_pool
+}
+
 #[actix_rt::test]
 async fn health_check_works() {
-    //Arrange
+    // Arrange
     let app = spawn_app().await;
-
-    // We need to bring in `reqwest`
-    // to perform HTTP requests against our application.
     let client = reqwest::Client::new();
 
-    //Act
+    // Act
     let response = client
+        // Use the returned application address
         .get(&format!("{}/health_check", &app.address))
         .send()
         .await
-        .expect("Failed to call health check endpoint");
-    //Assert
+        .expect("Failed to execute request.");
+
+    // Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
 
 #[actix_rt::test]
-async fn subscribe_return_200_for_valid_form_data() {
-    //Arrange
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Arrange
     let app = spawn_app().await;
     let client = reqwest::Client::new();
-    let body = "name=saif&email=test124%40gmail.com";
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
 
-    //Act
+    // Act
     let response = client
         .post(&format!("{}/subscriptions", &app.address))
         .header("Content-Type", "application/x-www-form-urlencoded")
         .body(body)
         .send()
         .await
-        .expect("Failed to execute request");
-    //Assert
+        .expect("Failed to execute request.");
+
+    // Assert
     assert_eq!(200, response.status().as_u16());
-    let saved = sqlx::query!("SELECT email, name from Subscriptions")
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
         .fetch_one(&app.db_pool)
         .await
-        .expect("Failed to query saved Subscription");
-    assert_eq!(saved.name, "saif");
-    assert_eq!(saved.email, "test124@gmail.com")
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
+
 #[actix_rt::test]
 async fn subscribe_returns_a_400_when_data_is_missing() {
     // Arrange
@@ -85,6 +106,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
         ("email=ursula_le_guin%40gmail.com", "missing the name"),
         ("", "missing both name and email"),
     ];
+
     for (invalid_body, error_message) in test_cases {
         // Act
         let response = client
@@ -94,6 +116,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
             .send()
             .await
             .expect("Failed to execute request.");
+
         // Assert
         assert_eq!(
             400,
