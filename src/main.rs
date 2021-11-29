745ecc4770cbabf7475b222c8542b0bef99bb78b
saif.husain <saif.husain@experian.com>
2021-11-29 05:31:32 -0500

    (refactor):add telemetry module to initialize telemetry

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,12 @@
 use devapi::configuration;
+use devapi::telemetry::{get_subscriber, init_subscriber};
 use sqlx::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // Redirect all `log`'s events to our subscriber
-    LogTracer::init().expect("Failed to set logger");
-    // We are falling back to printing all spans at info-level or above
-    // if the RUST_LOG environment variable has not been set.
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new(
-        "devapi".into(),
-        // Output the formatted spans to stdout.
-        std::io::stdout,
-    );
-    // The `with` method is provided by `SubscriberExt`, an extension
-    // trait for `Subscriber` exposed by `tracing_subscriber`
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-    // `set_global_default` can be used by applications to specify
-    // what subscriber should be used to process spans.
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    let subscriber = get_subscriber("devapi".into(), "info".into());
+    init_subscriber(subscriber);
     let config = configuration::get_configuration().expect("Failed to read configuration");
     let connection_string = config.database.connection_string();
     let connection_pool = PgPool::connect(&connection_string)
