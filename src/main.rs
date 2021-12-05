b2aefaa6bd31b3c95a42f55fb4d98ddae8d6b20e
saif.husain <saif.husain@experian.com>
2021-12-05 02:06:37 -0500

    (refactor): update env to work with gcloud

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,13 @@ use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let port = std::env::var("PORT").expect("PORT ENV not set");
     let subscriber = get_subscriber("devapi".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
     let config = configuration::get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPoolOptions::new()
         .connect_timeout(std::time::Duration::from_secs(2))
         .connect_lazy_with(config.database.with_db());
-    let address = format!("{}:{}", config.application.host, port);
+    let address = format!("{}:{}", config.application.host, config.application.port);
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
     devapi::startup::run(listener, connection_pool)?.await
 }
