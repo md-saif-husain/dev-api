c94089cd5a4af98099243ed03f46e53729bb6c68
saif.husain <saif.husain@experian.com>
2021-12-07 03:25:55 -0500

    (fix): return Http 400 for failed subscriber name validation

--- src/routes/subscription.rs
+++ src/routes/subscription.rs
@@ -22,9 +22,14 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     // `web::Form` is a wrapper around `FormData`
     // `form.0` gives us access to the underlying `FormData`
 
+    let name = match SubscriberName::parse(form.0.name) {
+        Ok(name) => name,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
+
     let new_subscriber = NewSubscriber {
         email: form.0.email,
-        name: SubscriberName::parse(form.0.name).expect("Name validation failed"),
+        name: name,
     };
     // `Result` has two variants: `Ok` and `Err`.
     // The first for successes, the second for failures.
