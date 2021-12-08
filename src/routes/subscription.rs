ab2f5216ce06ec642250eddb68983d461896c309
MD SAIF  HUSAIN <70682968+md-saif-husain@users.noreply.github.com>
2021-12-08 11:25:32 +0530

    Validation for Post New Subscriber API (#17)
    * test:post api subscription validation
    
    * feat: domain to handle validation check
    
    * refactor: db configuration
    
    * (test): domain unit test
    
    * (fix): return Http 400 for failed subscriber name validation
    
    * (refactor):seperate domain model file
    
    * (test): unit test for subscriber email domain
    
    * (lint):fix lint issue
    
    * (test):property based testing for subscriber_email using quickcheck
    
    * (test): fix integration test
    
    * (refactor): implement tryform to parse form data to domain model
    
    Co-authored-by: saif.husain <saif.husain@experian.com>

--- src/routes/subscription.rs
+++ src/routes/subscription.rs
@@ -1,7 +1,17 @@
+use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
 use actix_web::{web, HttpResponse};
 use serde::Deserialize;
 use sqlx::PgPool;
+use std::convert::{TryFrom, TryInto};
 
+impl TryFrom<FormData> for NewSubscriber {
+    type Error = String;
+    fn try_from(value: FormData) -> Result<Self, Self::Error> {
+        let name = SubscriberName::parse(value.name)?;
+        let email = SubscriberEmail::parse(value.email)?;
+        Ok(Self { email, name })
+    }
+}
 #[derive(Deserialize)]
 #[allow(dead_code)]
 pub struct FormData {
@@ -18,24 +28,37 @@ pub struct FormData {
     )
     )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    // `web::Form` is a wrapper around `FormData`
+    // `form.0` gives us access to the underlying `FormData`
+
+    let new_subscriber = match form.0.try_into() {
+        Ok(parse_data) => parse_data,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
     // `Result` has two variants: `Ok` and `Err`.
     // The first for successes, the second for failures.
     // We use a `match` statement to choose what to do based
     // on the outcome.
     // We will talk more about `Result` going forward!
-    match insert_subscriber(&pool, &form).await {
+    match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
     }
 }
 
-#[tracing::instrument(name = "Saving new Subscriber into datbase", skip(form, pool))]
-pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+#[tracing::instrument(
+    name = "Saving new Subscriber into datbase",
+    skip(new_subscriber, pool)
+)]
+pub async fn insert_subscriber(
+    pool: &PgPool,
+    new_subscriber: &NewSubscriber,
+) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)"#,
         uuid::Uuid::new_v4(),
-        form.email,
-        form.name,
+        new_subscriber.email.as_ref(),
+        new_subscriber.name.as_ref(),
         chrono::Utc::now()
     )
     .execute(pool)
