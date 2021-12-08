20ce648d9ad840d888861960c2e2c8ef2228299c
saif.husain <saif.husain@experian.com>
2021-12-07 23:53:06 -0500

    (test):property based testing for subscriber_email using quickcheck

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -28,6 +28,21 @@ mod tests {
     use claim::assert_err;
     use fake::faker::internet::en::SafeEmail;
     use fake::Fake;
+
+    // Both `Clone` and `Debug` are required by `quickcheck`
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
+            let email = SafeEmail().fake_with_rng(g);
+            Self(email)
+        }
+    }
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        dbg!(&valid_email.0);
+        SubscriberEmail::parse(valid_email.0).is_ok()
+    }
     #[test]
     fn empty_string_is_rejected() {
         let email = "".to_string();
